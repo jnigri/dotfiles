7e09bcd4543e7a101e47042bbb58aa0377c93822
Julien Nigri <julien@nigri.dev>
2026-06-24 11:20:17 +0200

    change style in nvim from catpuccin to 0x96f

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,15 +1,16 @@
 return {
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    opts = {
-      flavour = "mocha",
-    },
+    "filipjanevski/0x96f.nvim",
+    name = "0x96f",
+    priority = 1000,
+    config = function()
+      require("0x96f").setup()
+    end,
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
+      colorscheme = "0x96f",
     },
   },
 }
